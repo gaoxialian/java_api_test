edddf3fe783a28d0a0e08611e7b3d5fda769cce3
gaoxialian <1253555947@qq.com>
2020-06-11 07:33:17 +0800

    first commit

---
+++ charpter7/readme.txt
@@ -0,0 +1,3 @@
+java -jar moco-runner-1.1.0-standalone.jar http -p 8000 -c startupget.json
+下载地址：
+https://repo1.maven.org/maven2/com/github/dreamhead/moco-runner/
---
+++ charpter7/src/main/java/com/api/TestApi.java
@@ -0,0 +1,17 @@
+package com.api;
+
+import org.testng.annotations.Test;
+import sun.net.www.http.HttpClient;
+
+/**
+ * Created by gaoxl on 2020/6/6.
+ */
+public class TestApi {
+
+    @Test
+    public void test(){
+        HttpGet get = new HttpGet();
+        HttpClient client = new DefaultHttpClient();
+
+    }
+}
